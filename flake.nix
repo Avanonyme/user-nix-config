89cb59e590464540a6ec0a571a73db0fe56c4e26
Victor Bégin <50751186+Avanonyme@users.noreply.github.com>
2026-03-02 14:35:04 -0500

    added modules from https://github.com/vic/vix/ community repo. Couldn't figure out how to export it, will do later (maybe requires denful capabilities which are not there yet)

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     stylix.url = "github:danth/stylix";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs = {

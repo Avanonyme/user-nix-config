02b218a622163593c01f0283de29cd5d9bec663b
Victor Bégin <50751186+Avanonyme@users.noreply.github.com>
2026-04-20 19:00:26 -0400

    declared partition in /data

--- modules/aspects/modules/disko-config.nix
+++ modules/aspects/modules/disko-config.nix
@@ -85,9 +85,35 @@
               "com.sun:auto-snapshot" = "true";
               mountpoint = "legacy"; # mounted via fileSystems in boreal.nix, not ZFS auto-mount
             };
+            datasets = {
+              encrypted = {
+                type = "zfs_fs";
+                options = {
+                  encryption = "aes-256-gcm";
+                  keyformat = "passphrase";
+                  keylocation = "prompt";
+                  compression = "zstd";
+                  mountpoint = "legacy";
+                };
+              };
+            };
             postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^data@blank$' || zfs snapshot data@blank";
           };
         };
+      };
+      boot.zfs.requestEncryptionCredentials = [ "data/encrypted" ]; # prompt for encryption credentials at boot for the encrypted dataset
+      # you can decrypt it after boot using
+      # zfs load-key data/encrypted
+      # zfs mount data/encrypted
+      # and encrypt again using
+      # zfs unmount data/encrypted
+      # zfs unload-key data/encrypted
+
+      # declare 
+      fileSystems."/data/game" = {
+        device = "data/game";
+        fsType = "zfs";
+        options = [ "zfsutil" ];
       };
     };
   };

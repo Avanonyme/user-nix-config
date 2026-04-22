e949b93bbd7b96c62597b2346c8ba43243aee963
avanonyme <avanix26@protonmail.com>
2026-04-22 17:43:50 -0400

    added noctalia config

--- modules/aspects/users/gamer.nix
+++ modules/aspects/users/gamer.nix
@@ -7,6 +7,8 @@
       den.provides.define-user
       (den.provides.user-shell "fish")
       den.aspects.gaming
+      den.aspects.noctalia-desktop
+      den.aspects.zen-browser
 
     ];
     nixos = { ... }: {
